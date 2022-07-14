d4322bfcbbdbac2f876dfa6fa772bdeb03138b47
lucasstarsz <andrewrcdey@gmail.com>
2022-07-13 23:16:21 -0400

    structure setup

---
+++ hserver/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java")
+}
+
+group = "tech.lucasz"
+version = "0.0.1"
+
+sourceSets {
+    main {
+        java.setSrcDirs(listOf("main/java"))
+        resources.setSrcDirs(listOf("main/resources"))
+    }
+
+    test {
+        java.setSrcDirs(listOf("test/java"))
+        resources.setSrcDirs(listOf("test/resources"))
+    }
+}
+
+repositories.maven { setUrl("https://jitpack.io/") }
+repositories.mavenCentral()
+
+dependencies.implementation(libs.bundles.fastj)
+
+dependencies.testImplementation(dependencies.platform("org.junit:junit-bom:5.8.2"))
+dependencies.testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+dependencies.testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "party-house"
+include("hcore", "hclient", "hserver", "network")
+enableFeaturePreview("VERSION_CATALOGS")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
